f0fa533369524a3472e9d4013825b4257630d117
Shreyash Mishra <72780721+shre0079@users.noreply.github.com>
2026-03-27 03:24:48 +0530

    Conact App Navbar design on hold at 36mins timestamp

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,63 @@
+import { AiFillPlusCircle } from "react-icons/ai";
 import Navbar from "./components/Navbar";
+import { FiSearch } from "react-icons/fi";
+import { useEffect, useState } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import { HiOutlineUserCircle } from "react-icons/hi";
 
-const App = () => {
+function App() {
+  const [contacts, setContacts] = useState([]);
+
+  useEffect(() => {
+    const getContacts = async () => {
+      try {
+        const contactsRef = collection(db, "contacts");
+
+        onSnapshot(contactsRef, (snapshot) => {
+          const contactLists = snapshot.docs.map((doc) => {
+            return {
+              id: doc.id,
+              ...doc.data(),
+            };
+          });
+          setContacts(contactLists);
+          return contactLists;
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    getContacts();
+  }, []);
+
   return (
-    <div><Navbar/></div>
-  )
+    <div className="mx-auto max-w-[370px] px-4">
+      <Navbar />
+      <div className="flex gap-2">
+        <div className="relative flex flex-grow items-center">
+          <FiSearch className="absolute ml-1 text-3xl text-white" />
+          <input
+            type="text"
+            className=" h-9.5 flex-grow rounded-md border border-white bg-transparent pl-9 text-white"
+          />
+        </div>
+
+        <AiFillPlusCircle className="text-4xl text-white cursor-pointer" />
+      </div>
+      <div>
+        {contacts.map((contact) => (
+          <div key={contact.id}>
+            <HiOutlineUserCircle />
+            <div>
+              <h2></h2>
+              <p></p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,10 @@
-import React from "react";
-
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="h-[60px] bg-white my-4 rounded-lg flex justify-center items-center gap-2 text-xl font-medium">
+      <img src="/logos_firebase.png"></img>
+      <h1> Firebase Contact App</h1>
+    </div>
+  );
 };
 
 export default Navbar;
