c1b78292be504ec253b451b97887fb757754a16c
Shreyash Mishra <72780721+shre0079@users.noreply.github.com>
2026-05-29 17:53:47 +0530

    navbar comp

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,8 @@
 const Navbar = () => {
   return (
-    <div className="h-[60px] bg-white my-4 rounded-lg flex justify-center items-center gap-2 text-xl font-medium">
-      <img src="/logos_firebase.png"></img>
-      <h1> Firebase Contact App</h1>
+    <div className="my-4 flex h-[60px] items-center justify-center gap-2 rounded-lg bg-white text-xl font-medium">
+      <img src="/logos_firebase.png" />
+      <h1>Firebase Contact App</h1>
     </div>
   );
 };
